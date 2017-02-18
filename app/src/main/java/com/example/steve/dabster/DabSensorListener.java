ebcc6342d9d8bcc60aef68a4bd1a0643ee2ddcdc
Erick Leon <eri.lewo@hotmail.com>
2017-02-18 16:11:56 -0500

    fgkdjsfl

--- app/src/main/java/com/example/steve/dabster/DabSensorListener.java
+++ app/src/main/java/com/example/steve/dabster/DabSensorListener.java
@@ -12,15 +12,118 @@ import android.widget.TextView;
 public class DabSensorListener implements SensorEventListener{
     private TextView out;
 
-    //enum ySTATE = {yWAIT };
+    enum ySTATE{yWAIT, yFALL1, yRISE1, yDAB};
+    ySTATE state1 = ySTATE.yWAIT;
+    enum zSTATE{zWAIT, zFALL1, zRISE1, zFALL2, zDAB};
+    zSTATE state2 = zSTATE.zWAIT;
+    boolean dab;
+
+    final float [] yThresh = {0,0,0};
+    final float [] zThresh = {0,0,0,0};
+
+    final int SAMPLEDEFAULT = 100;
+    int sampleCounter = SAMPLEDEFAULT;
+
+    private float x;
+    private float y;
+    private float z;
+
+    private float [][] array = new float[3][100];//3x100 array
+
     public DabSensorListener(TextView dab) {
         out = dab;
     }
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
+    public void onSensorChanged(SensorEvent se) {
+        x = se.values[0];
+        y = se.values[1];
+        z = se.values[2];
+    }
+
+    public void determine(float[] values){
+        //filtering
+        for(int i = 1; i < 100; i++){
+            array[i-1][0] = array[i][0];
+            array[i-1][1] = array[i][1];
+            array[i-1][2] = array[i][2];
+        }
+
+        //adding new value to last element FILTERING
+        array[99][0] += ( values[0] - array[99][0] ) / 10;
+        array[99][1] += ( values[1] - array[99][1] ) / 10;
+        array[99][2] += ( values[2] - array[99][2] ) / 10;
+
+        StateChange();
 
     }
+    public void StateChange(){
+        float dy = array[1][99] - array[1][98];
+        float dz = array[2][99] - array[2][98];
+
+        switch(state1){//y values
+            case yWAIT:
+                sampleCounter = SAMPLEDEFAULT;
+
+                if(dy > 0){
+                    if(array[1][99] > yThresh[0]){
+                        state1 = ySTATE.yFALL1;
+                    }
+                }
+                break;
+            case yFALL1:
+                if(dy < 0){
+                    if(array[1][99] < yThresh[1]){
+                        state1 = ySTATE.yRISE1;
+                    }
+                }
+                break;
+            case yRISE1:
+                if(dy > 0){
+                    if(array[1][99] > yThresh[1]){
+                        state1 = ySTATE.yDAB;
+                    }
+                }
+                break;
+            default:
+                state1 = ySTATE.yDAB;
+        }
+
+        switch(state2){//z values
+            case zWAIT:
+                if(dz > 0){
+                    if(array[2][99] > zThresh[0]){
+                        state2 = zSTATE.zFALL1;
+                    }
+                }
+                break;
+            case zFALL1:
+                if(dz < 0){
+                    if(array[2][99] < zThresh[1]){
+                        state2 = zSTATE.zRISE1;
+                    }
+                }
+                break;
+            case zRISE1:
+                if(dz>0){
+                    if(array[2][99] > zThresh[2]){
+                        state2 = zSTATE.zFALL2;
+                    }
+                }
+                break;
+            case zFALL2:
+                if(dz < 0){
+                    if(array[2][99] < zThresh[3]){
+                        state2 = zSTATE.zDAB;
+                    }
+                }
+                break;
+            default:
+                state2 = zSTATE.zDAB;
+        }
+    }
+
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {}
--- app/src/main/java/com/example/steve/dabster/MainActivity.java
+++ app/src/main/java/com/example/steve/dabster/MainActivity.java
@@ -11,22 +11,38 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Sensor Manager Information
+    SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    Sensor accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION); //3 values
+    TextView dab = new TextView(getApplicationContext());
+    SensorEventListener AccEvent = new DabSensorListener(dab);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Sensor Manager Information
-        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        Sensor accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION); //3 values
 
         LinearLayout ll = (LinearLayout) findViewById(R.id.activity_main);;
-        TextView dab = new TextView(getApplicationContext());
+        dab.setText("");
         dab.setTextColor(Color.BLACK);
         ll.addView(dab);
 
-        SensorEventListener AccEvent = new DabSensorListener(dab);
+        sensorManager.registerListener(AccEvent, accSensor, SensorManager.SENSOR_DELAY_GAME);
+
+
+    }
+    public void onPause()
+    {
+        super.onPause();
+        sensorManager.unregisterListener(AccEvent, accSensor);
+    }
+    public void onResume()
+    {
+        super.onResume();
         sensorManager.registerListener(AccEvent, accSensor, SensorManager.SENSOR_DELAY_GAME);
     }
 }
